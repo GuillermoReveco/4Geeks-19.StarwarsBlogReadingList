f6ac269e7b120b638a64b41c98d061c92ca2708d
GuillermoReveco <guillermo.reveco@gmail.com>
2022-11-09 20:42:21 -0300

    19.StarwarsBlogReadingList Modifique Home.js

--- src/componentes/Home.js
+++ src/componentes/Home.js
@@ -13,35 +13,8 @@ const Home = () => {
     const { arrPeople, setArrPeople } = useContext(arrPeopleContext);
     const { arrPlanets, setArrPlanets } = useContext(arrPlanetsContext);
 
-    // const [peopleData, setPeopleData] = useState([]);
-    // const [planetsData, setPlanetsData] = useState([]);
-    // const [isCall, setIsCall] = useState(true);
-    // const [carga, setCarga] = useState(true);
-
-    // const [dataPeople, setDataPeople] = useState();
-    // const [dataPlanets, setDataPlanets] = useState();
     const [imgPeople, setImgPeople] = useState('https://starwars-visualguide.com/assets/img/characters/@.jpg');
     const [imgPlanets, setImgPlanets] = useState('https://starwars-visualguide.com/assets/img/planets/@.jpg');
-
-    // useEffect(() => {
-    //     if (isCall) {
-    //         // consultaPeople();
-    //         consultaPlanets();
-    //         // console.log('peopleData: ', peopleData);
-    //         console.log('useEffect - iscall: ', isCall);
-    //         setIsCall(false);
-    //     }
-    // }, [isCall]);
-
-    // useEffect(() => {
-    //     console.log('UseEffect - peopleData: ', peopleData);
-    //     console.log('UseEffect - planetsData: ', planetsData);
-    //     if ((peopleData.length > 1) || (planetsData.length > 1)) {
-    //         setArrPeople(peopleData);
-    //         setArrPlanets(planetsData);
-    //         setCarga(false);
-    //     }
-    // }, [peopleData, planetsData]);
 
     //People
     useEffect(() => {
@@ -144,123 +117,6 @@ const Home = () => {
 
     }
 
-    //     const consultaPeople = () => {
-    //         // let character;
-    //         var requestOptions = {
-    //             method: 'GET',
-    //             redirect: 'follow'
-    //         };
-    //         fetch('https://swapi.dev/api/people', requestOptions)
-    //             .then(response => response.json())
-    //             .then(result => {
-    //                 console.log('consultaPeople - result', result)
-    //                 //   console.log('consultaPeople - cantidad:', result.length);
-    //                 var dataPerson = result.results;
-    //                 console.log('consultaPeople - results:', dataPerson);
-    //                 for (const key in dataPerson) {
-    //                     // console.log('consultaPeople - key:', key);
-    //                     // console.log('consultaPeople - url:', dataPerson[key].url);
-    //                     // character = consultaDetPeople(dataPerson[key].url, dataPerson[key].uid, dataPerson[key].name);
-    //                     consultaDetPeople(key, dataPerson);
-    //                 }
-    //                 //   setCarga(false);
-    //                 //   setPeopleData(dataPerson);
-    //             })
-    //             .catch(error => console.log('consultaPeople - error', error))
-
-    //     }
-    //     const consultaDetPeople = (index, item) => {
-    //         let people = [];
-    //         // let image = 'https://starwars-visualguide.com/assets/img/planets/@.jpg';
-    //         let image = 'https://starwars-visualguide.com/assets/img/characters/@.jpg';
-
-    //         // var requestOptions = {
-    //         //     method: 'GET',
-    //         //     redirect: 'follow'
-    //         //   };
-    //         //   fetch(url, requestOptions)
-    //         //     .then(response => response.json())
-    //         //     .then(result => {
-    //         //       console.log('consultaDetPeople - result', result)
-    //         //     //   console.log('consultaPeople - cantidad:', result.length);
-    //         //       var dataDetPerson = result.result;
-    //         //       console.log('consultaDetPeople - results:', dataDetPerson);
-    //         //       console.log('consultaDetPeople - description:', dataDetPerson.description);
-    //         //       console.log('consultaDetPeople - properties:', dataDetPerson.properties);
-    //         //       console.log('consultaDetPeople - eye_color:', dataDetPerson.properties.eye_color);
-    //         let arr = item[index].url.split('/');
-    //         let id = arr[arr.length - 2];
-    //         console.log('consultaDetPeople - id:', id);
-    //         people.push({
-    //             'id': id,
-    //             'name': item[index].name,
-    //             'img': image.replace('@', `${id}`),
-    //             'gender': item[index].gender,
-    //             'hair_color': item[index].hair_color,
-    //             'eye_color': item[index].eye_color,
-    //             'birth_year': item[index].birth_year,
-    //             'height': item[index].height,
-    //             'skin_color': item[index].skin_color,
-    //         });
-    //         console.log('consultaDetPeople - people:', people);
-    //         setPeopleData((p) => [...p, people[0]]);
-    //         //   for (const key in dataPerson) {
-    //         //     console.log('consultaPeople - key:', key);
-    //         //     console.log('consultaPeople - url:', dataPerson[key].url);
-    //         //   }
-    //         //   setPeopleData(dataPerson);
-    //         //     })
-    //         //     .catch(error => console.log('consultaPeople - error', error));
-
-    //         // return people;
-    //     }
-    // //Planets
-    // const consultaPlanets = () => {
-    //     var requestOptions = {
-    //         method: 'GET',
-    //         redirect: 'follow'
-    //     };
-    //     fetch('https://swapi.dev/api/planets', requestOptions)
-    //         .then(response => response.json())
-    //         .then(result => {
-    //             console.log('consultaPlanets - result', result)
-    //             var dataPlanets = result.results;
-    //             console.log('consultaPlanets - results:', dataPlanets);
-    //             for (const key in dataPlanets) {
-    //                 consultaDetPlanets(key, dataPlanets);
-    //             }
-    //             //   setCarga(false);
-    //             //   setPeopleData(dataPerson);
-    //         })
-    //         .catch(error => console.log('consultaPeople - error', error))
-
-    // }
-    // const consultaDetPlanets = (index, item) => {
-    //     let planets = [];
-    //     // let image = 'https://starwars-visualguide.com/assets/img/planets/@.jpg';
-    //     let image = 'https://starwars-visualguide.com/assets/img/planets/@.jpg';
-
-    //     let arr = item[index].url.split('/');
-    //     let id = arr[arr.length - 2];
-    //     console.log('consultaDetPlanets - id:', id);
-    //     let img = image.replace('@', `${id}`);
-    //     if(id == 1){
-    //         img = 'https://starwars-visualguide.com/assets/img/big-placeholder.jpg'
-    //     }
-    //     planets.push({
-    //         'id': id,
-    //         'name': item[index].name,
-    //         'img': img,
-    //         'population': item[index].population,
-    //         'terrain': item[index].terrain,
-    //         'climate': item[index].climate,
-    //         'orbital_period': item[index].orbital_period,
-    //         'rotation_period': item[index].rotation_period,
-    //         'diameter': item[index].diameter,
-    //     });
-    //     console.log('consultaDetPlanets - planets:', planets);
-    //     setPlanetsData((p) => [...p, planets[0]]);
-    // }
 
     return (
         <>
@@ -327,6 +183,29 @@ const Home = () => {
 
                     </div>
                 </div>
+
+                <div className='container mt-3'>
+                    <nav aria-label="Page navigation example">
+                {
+                    arrPlanets?
+                    (
+
+                        <ul className="pagination">
+                            <li className={arrPlanets.previous? 'page-item' : 'page-item disabled'} ><a className="page-link" href="#" onClick={()=>getPrevPlanets()}>Previous</a></li>
+                            <li className={arrPlanets.next? 'page-item' : 'page-item disabled'}  ><a className="page-link" href="#" onClick={()=>getNextPlanets()}>Next</a></li>
+                        </ul>
+
+                    ):(
+
+                        <ul className="pagination">
+                        </ul>
+
+                    )
+                }
+
+                    </nav>
+                </div>
+
                 <div className='container mt-4'>
                     <div className='character row-cols-md-5 g-2'>
                         {
@@ -378,27 +257,6 @@ const Home = () => {
                     </div>
                 </div>
                 
-                <div className='container mt-3'>
-                    <nav aria-label="Page navigation example">
-                {
-                    arrPlanets?
-                    (
-
-                        <ul className="pagination">
-                            <li className={arrPlanets.previous? 'page-item' : 'page-item disabled'} ><a className="page-link" href="#" onClick={()=>getPrevPlanets()}>Previous</a></li>
-                            <li className={arrPlanets.next? 'page-item' : 'page-item disabled'}  ><a className="page-link" href="#" onClick={()=>getNextPlanets()}>Next</a></li>
-                        </ul>
-
-                    ):(
-
-                        <ul className="pagination">
-                        </ul>
-
-                    )
-                }
-
-                    </nav>
-                </div>
 
             </Layout>
         </>
